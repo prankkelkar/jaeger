6bbc68028ff963ba9cece5bbba1c692dba021124
Prasanna Kelkar <Prasanna.Kelkar@ibm.com>
2019-12-17 14:09:13 +0530

    Reducing extra changes. PR

--- scripts/travis/install-crossdock-deps.sh
+++ scripts/travis/install-crossdock-deps.sh
@@ -5,13 +5,8 @@ set -e
 docker version
 
 # Install docker-compose
-if [[ $(uname -m) == 's390x' ]]; then
-    echo "in s390x branch"
-else
-    sudo rm -f /usr/local/bin/docker-compose
-    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
-    chmod +x /tmp/docker-compose
-    sudo mv /tmp/docker-compose /usr/local/bin
-fi  
-
+sudo rm -f /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
+chmod +x /tmp/docker-compose
+sudo mv /tmp/docker-compose /usr/local/bin
 docker-compose version
